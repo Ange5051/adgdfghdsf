5191c93684fcc939252f060382c538b19db90384
Ange5051 <147865401+Ange5051@users.noreply.github.com>
2024-10-29 11:15:47 +0200

    Add files via upload

---
+++ ангел/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. вывести в столбик 10 раз слово "Привет"
+            //int i = 1;
+            //while (i <= 10)
+            //{
+            //    Console.WriteLine("Привет");
+            //    i++;
+            //}
+
+            //Компьютерный практикум № 8
+            //Задача 1. Заполнить одномерный массив из 8 целых чисел с клавиатуры и вывести его на экран.
+            //int[] omas = new int[8];
+            //Console.Write("Заполнение одномерного масива");
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    Console.Write($"omas[{i}]=");
+            //    omas[i]= int.Parse (Console.ReadLine());
+            //}
+            //Console.WriteLine("Вывод одномерного масива:");
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //Console.ReadLine();
+
+            //Задача 2. Составьте программу ввода (без клавиатуры) и вывода массива: 5, -2, 3, -6, 7, 9, -4, 8, 1, 2, 7, -3.
+            //int[] omas = new int[12] { 5, -2, 3, -6, 7, 9, -4, 8, 1, 2, 7, -3 };
+            //Console.WriteLine("Вывод одномерного масива:");
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //Console.ReadLine();
+
+            //Задача 3. Массив предназначен для хранения значений ростов двенадцати человек.
+            //С помощью датчика случайных чисел заполнить массив целыми значениями, лежащими в диапазоне от 163 до 190 включительно.
+            //int[] omas = new int[12];
+            //Random rnd = new Random();
+            //Console.Write("Заполнение одномерного масива");
+            //for (int i = 0; i < omas.Length; i++) 
+            //{
+            //    omas[i] = rnd.Next(168,190);
+            //}
+            //Console.ReadLine();
+
+            //int[] omas = new int[12];
+            //Random rnd = new Random();
+            //Console.Write("Заполнение одномерного масива");
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    omas[i] = rnd.Next(5, 25);
+            //}
+            //Console.WriteLine("Вывод одномерного масива:");
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //int suma  = 0;
+            //for (int i = 0; i < omas.Length;i++)
+            //{
+            //    //suma = suma + omas[i];
+            //    suma += omas[1];
+            //}
+            //Console.WriteLine($"Среднаяя температура ровна {suma/ omas.Length}");
+
+            //Console.ReadKey();
+        }
+    }
+}
